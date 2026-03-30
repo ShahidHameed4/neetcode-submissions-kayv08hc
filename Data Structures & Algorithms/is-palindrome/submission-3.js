3518b37aee94ebadc75b6f871f5fe29b4fe615c4
Shahid Hameed <90132007+ShahidHameed4@users.noreply.github.com>
2026-03-30 23:31:59 +0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+
+        let arr = s.toLowerCase().split(/[^a-zA-Z0-9]+/).join('').split('')
+        let j = arr.length - 1
+        for (let i=0; i< arr.length; i++){
+            if(i>=j){ return true}
+            else if(arr[i]!=arr[j]) return false
+            j--
+        }
+        if (s.length == 0) return false
+        else return true
+
+    }
+}
